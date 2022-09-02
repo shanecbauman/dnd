c18c7e002a185b51590219d3dfcc8dee3c2051f1
Shane Bauman <shanebauman11@gmail.com>
2022-09-01 20:57:24 -0600

    Adding base64 converter

---
+++ base64_converter.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+import requests
+import base64
+from bs4 import BeautifulSoup
+
+url = input("Direct URL to image:")
+url_bytes = url.encode('ascii')
+base64_bytes = base64.b64encode(url_bytes)
+base64_url = base64_bytes.decode('ascii')
+
+resp = requests.get(f'https://token.otfbm.io/meta/{base64_url}', timeout=15)
+soup = BeautifulSoup(resp.content, 'html.parser')
+print(soup.body.string)
