67ed9bf8fb10481a3770797aeedc21f9e1460759
Shane Bauman <shanebauman11@gmail.com>
2022-11-04 21:24:41 -0600

    Added redbrand rukffians

--- base64_converter.py
+++ base64_converter.py
@@ -9,6 +9,6 @@ url_bytes = url.encode('ascii')
 base64_bytes = base64.b64encode(url_bytes)
 base64_url = base64_bytes.decode('ascii')
 
-resp = requests.get(f'https://token.otfbm.io/meta/{base64_url}', timeout=15)
+resp = requests.get(f'https://token.otfbm.io/meta/{base64_url}', timeout=30)
 soup = BeautifulSoup(resp.content, 'html.parser')
 print(soup.body.string)
